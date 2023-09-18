a2612a609c8bc46432b9af50866536336534f462
Akash <akashkulkarni4444@gmail.com>
2023-09-19 03:24:40 +0530

    changed a class property

--- src/components/Content.js
+++ src/components/Content.js
@@ -7,7 +7,7 @@ import Revised from "./Content/Revised";
 
 const Content = () => {
   return (
-    <div className={`w-full p-5 h-screen`}>
+    <div className={`w-full p-5 min-h-screen`}>
       <Title />
       <div className="mt-4 w-full overflow-x-auto">
         <div className="flex gap-10 min-w-[95rem]">
